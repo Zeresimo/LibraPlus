17a0f699cf0d9f760b0bf73a9d70142e12aee1ab
Leo Castillo <castillo.leorenzo@gmail.com>
2025-03-22 21:22:02 -0700

    Function Definition
    Added function definitions, removed main function as it is not needed in an implementation file.

--- src/LibraBook.cpp
+++ src/LibraBook.cpp
@@ -2,7 +2,26 @@
 #include "LibraBook.h"
 using namespace std;
 
-int main()
+LibBook::LibBook(const std::string &title, const std::string &author, int year)
 {
-    
+}
+
+void LibBook::setTitle(const std::string &title)
+{
+}
+
+void LibBook::setAuthor(const std::string &author)
+{
+}
+
+int LibBook::getYear() const
+{   
+}
+
+void LibBook::setYear(int year)
+{
+}
+
+void LibBook::displayBookInfo() const
+{
 }

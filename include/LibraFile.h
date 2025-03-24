6b5432e0f8f79e7d179c6a2138ad9e9dc34b4bf0
Leo Castillo <castillo.leorenzo@gmail.com>
2025-03-23 22:25:59 -0700

    File Open
    Created LibFile Class and Members, defined default file opening and user-defined file opening

--- include/LibraFile.h
+++ include/LibraFile.h
@@ -1,5 +1,22 @@
 #ifndef LIBRAFILE_H
 #define LIBRAFILE_H
 
+#include <fstream>
+#include <string>
+#include <iostream>
 
+class LibFile 
+{
+public: // Methods or variables that can be accessed outside the class
+
+    // Constructor: initializes a file with a default filename
+    LibFile(); 
+
+    // Constructor: initializes a file with a specified filename
+    LibFile(const std::string& filePath);
+
+private: // Methods or variables that can be accessed within the class
+    std::string filename; // Name of file
+    std::fstream filestrm; // File stream
+};
 #endif // LIBRAFILE_H
--- src/LibraFile.cpp
+++ src/LibraFile.cpp
@@ -1,3 +1,26 @@
 #include <iostream>
+#include <fstream>
+#include <string>
 #include "LibraFile.h"
 
+LibFile::LibFile() : filename("books.txt")
+{
+    filestrm.open(filename, std::ios::in); // Open file with filename books.txt
+
+    if (!filestrm) // If file does not exist
+    {
+        filestrm.open(filename, std::ios::out); // Create file with filename books.txt
+        std::cout << "File created: " << filename << std::endl; // Inform User
+    }
+}
+
+LibFile::LibFile(const std::string& filePath) : filename(filePath)
+{
+    filestrm.open(filename, std::ios::in); // Open specified file
+
+    if (!filestrm) // If file does not exist
+    {
+        filestrm.open(filename, std::ios::out); // Create file with filename
+        std::cout << "File created: " << filename << std::endl; // Inform User
+    }
+}
